7bc7a18d4a1f2bb053f4489f9502f8c9c667cf8d
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2022-01-10 17:09:43 -0600

    Bootstrap Instaled

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Products } from "..";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+      <Products />
+      <Button>Default Button</Button>
+    </div>
+  );
+}
+
+export { App };
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import { App } from "./App/App.js";
+import { Products } from "./Products/Products.jsx";
+
+export { App, Products };
