05f0483a5fb7f1925e7b6814d657864cec294774
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2022-01-11 13:49:11 -0600

    Chapter 3: props, page 23

--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
+// Icons: react-icons
+import { IoIosStarOutline, IoIosStar } from "react-icons/io";
+
 function Rating({ rating }) {
   return (
     <div>
       <h1>Rating: {rating} </h1>
+      {rating >= 1 ? <IoIosStar /> : <IoIosStarOutline />}
+      {rating >= 2 ? <IoIosStar /> : <IoIosStarOutline />}
+      {rating >= 3 ? <IoIosStar /> : <IoIosStarOutline />}
+      {rating >= 4 ? <IoIosStar /> : <IoIosStarOutline />}
+      {rating >= 5 ? <IoIosStar /> : <IoIosStarOutline />}
     </div>
   );
 }
