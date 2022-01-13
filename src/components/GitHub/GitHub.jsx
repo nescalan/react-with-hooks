3b50eb3ec789d46dd44d8176745e9a29e8f72313
Nelson <nescalan@gmail.com>
2022-01-13 16:24:43 -0600

    Chapter 7: Display Results on Page, page 58

--- src/components/GitHub/GitHub.jsx
+++ src/components/GitHub/GitHub.jsx
@@ -1,22 +1,31 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Spinner from "react-bootstrap/Spinner";
+import { Card, Spinner } from "react-bootstrap";
 
 function GitHub() {
-  const [searchItem, setSearchItem] = useState("greg");
-  const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("nescalan");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     axios
-      .get(`https://api.github.com/search/users?q=${searchItem}`)
+      .get(`https://api.github.com/search/users?q=${searchTerm}`)
       .then((res) => {
         setData(res.data.items);
         setIsLoading(false);
       });
   }, []);
 
-  console.log(data);
+  const listUsers = data.map((user) => (
+    <Card key={user.id} style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={user.avatar_url} />
+      <Card.Body>
+        <Card.Title>User: {user.login}</Card.Title>
+        <Card.Subtitle>Id: {user.id} </Card.Subtitle>
+      </Card.Body>
+    </Card>
+  ));
+
   return (
     <div>
       {isLoading && (
@@ -24,6 +33,7 @@ function GitHub() {
           <span className="visually-hidden">Loading...</span>
         </Spinner>
       )}
+      {listUsers}
     </div>
   );
 }
