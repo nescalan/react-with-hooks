d2a8ac1323537049334eeb89a48107db0f593eaf
Nelson <nescalan@gmail.com>
2022-01-13 15:04:12 -0600

    Chapter 7: Fetching Data

--- src/components/GitHub/GitHub.jsx
+++ src/components/GitHub/GitHub.jsx
@@ -2,20 +2,21 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function GitHub() {
+  const [searchItem, setSearchItem] = useState("greg");
+  const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
-  const [searchItem, setSearchItem] = useState("");
 
   useEffect(() => {
     axios
       .get(`https://api.github.com/search/users?q=${searchItem}`)
       .then((res) => {
         setData(res.data.items);
+        setIsLoading(false);
       });
   }, []);
 
   console.log(data);
-
-  return <div></div>;
+  return <div>{isLoading && <h4>Getting Data..</h4>}</div>;
 }
 
 export { GitHub };
