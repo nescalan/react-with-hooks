a74cbc5ca89a47e1b0a6be384dd2c077339d65de
Nelson <nescalan@gmail.com>
2022-01-13 08:43:15 -0600

    Chapter 5: Jumbotron Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { JumbotronComponent } from "..";
 
 // Styles
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Products } from "..";
 
 function App() {
   return (
     <div>
-      <Products />
+      <JumbotronComponent>
+        <h1>
+          Using a series of utilities, you can create this jumbotron, just like
+          the one in previous versions of Bootstrap. Check out the examples
+          below for how you can remix and restyle it to your liking.
+        </h1>
+      </JumbotronComponent>
     </div>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import { App } from "./App/App.js";
+import { JumbotronComponent } from "./JumbotronComponent/JumbotronComponent.jsx";
 import { Product } from "./Product/Product";
 import { Products } from "./Products/Products.jsx";
 import { Rating } from "./Rating/Rating";
 
-export { App, Product, Products, Rating };
+export { App, JumbotronComponent, Product, Products, Rating };
